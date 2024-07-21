244b54d7b69483e94ed8b1e3728ad7af5b8450d1
Schnitzel <Schnitzel@LAPTOP>
2024-07-21 18:39:37 +0200

    Added error handling for undeleted files

--- cleaner.py
+++ cleaner.py
@@ -1,22 +1,44 @@
+"""os module"""
 import os
-import schedule
+import json
 import time
+import schedule
 
-directory_in_str = r"C:\Users\jeroe\OneDrive\Coding Projects\Python\Discord-To-Drive\temp"
+
+with open("config.json", "r", encoding="utf-8") as config_file:
+    config = json.load(config_file)
+
+directory_in_str = config["directory_to_remove"]
+time_to_remove = config["time_24h"]
+
 directory = os.fsencode(directory_in_str)
 
-filecount = 0
-
-def clean_temp_folder():
+def filenames():
+    print(f"Files in {config["directory_to_remove"]}:")
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
+        print(filename)
+
+
+def clean_temp_folder():
+    """Function to clean specified folder"""
+    
+    succesfull = False
+    filecount = 0
+    for file in os.listdir(directory):
+        filecount = filecount + 1
+        filename = os.fsdecode(file)
         full_path_to_file = os.path.join(directory_in_str, filename)
-        os.remove(full_path_to_file)
-        filecount + 1
-    print(f"Successfully ran clean-up script at 00:00, removed {filecount} files")
+        try:
+            os.remove(full_path_to_file)
+            succesfull = True
+        except OSError as error:
+            print(error)
+    if succesfull == True:
+        print(f"Successfully ran clean-up script at {time_to_remove}, removed {filecount} files")
 
-schedule.every().day.at("12:00").do(clean_temp_folder)
-
+schedule.every().day.at(time_to_remove).do(filenames)
+schedule.every().day.at(time_to_remove).do(clean_temp_folder)
 
 while True:
     schedule.run_pending()
